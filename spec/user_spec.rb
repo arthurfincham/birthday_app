79937f8c1b27fb3c7ec8e9960e7179c30ffd0277
Arthur Fincham <arthurfincham@me.com>
2021-10-06 16:46:10 +0100

    user spec refactor

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,13 +1,17 @@
 require 'user'
 
 describe User do
-  subject { described_class.new("Arthur", "1997-01-09")}
+  subject { described_class.new("Arthur", "1", 9)}
   
   it 'has a name' do
     expect(subject.name).to eq "Arthur"
   end
 
-  it 'has a birthday' do
-    expect(subject.birthday).to eq "1997-01-09"
+  it 'has a day' do
+    expect(subject.day).to eq 1
+  end
+
+  it 'has a month' do
+    expect(subject.month).to eq 9
   end
 end
