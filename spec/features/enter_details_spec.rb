f16ead0a0bb9af48c15c4e7b7278adce6962f8ea
Arthur Fincham <arthurfincham@me.com>
2021-10-07 11:33:32 +0100

    add name spec

--- spec/features/enter_details_spec.rb
+++ spec/features/enter_details_spec.rb
@@ -1,17 +1,7 @@
-feature 'Enter names' do
+feature 'Enter details' do
   scenario 'welcome message' do
     visit('/')
     expect(page).to have_content 'Please enter your name'
     expect(page).to have_content 'Please enter your birthday'
-  end
-
-  scenario 'submitting names' do
-    visit('/')
-    fill_in :name, with: 'Charlotte'
-    page.select '9', from: 'day'
-    expect(page).to have_select('day')
-    page.select 'Jan', from: 'month'
-    expect(page).to have_select('month')
-    click_button 'Submit'
   end
 end
